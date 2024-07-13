d166cf00746d10d9618505cc310fe81d182a23a5
Wilson <67292323+whl-usc@users.noreply.github.com>
2024-07-12 18:58:03 -0700

    Updated plot_readcount.py

--- plot_readcount.py
+++ plot_readcount.py
@@ -6,7 +6,6 @@ script:     plot_readcount.py
 
 This Python script plots gene counts in user-defined styles. 
 Graph can be log(2) transformed to find normal distribution.
-
 """
 # Define version
 __version__ = "1.0.0"
@@ -42,8 +41,7 @@ def readfile(file_path, cutoff=None, exclude_chromosomes=None):
         extracted_df = extracted_df[extracted_df['count'] > cutoff]
 
     if exclude_chromosomes is not None:
-        filtered_df = extracted_df[~extracted_df['chrom'].isin(
-            exclude_chromosomes)]
+        filtered_df = extracted_df[~extracted_df['chrom'].isin(exclude_chromosomes)]
     else:
         filtered_df = extracted_df
 
@@ -54,7 +52,9 @@ def plot_gene_counts(dataframes, output_file, log_transform=False):
     Plots histogram of gene counts from the dataframe, optionally
     log2-transformed.
     """
-    for dataframe in dataframes:
+    plt.figure(figsize=(12, 6))  
+
+    for dataframe, filename in dataframes:
         if log_transform:
             dataframe['log2_count'] = np.log2(dataframe['count'] + 1)
             count_data = dataframe['log2_count']
@@ -66,60 +66,61 @@ def plot_gene_counts(dataframes, output_file, log_transform=False):
             xlabel = 'Raw Expression Counts'
             title = 'Overlay of Raw Gene Counts'
             plt.xlim(0, 20000)
-            bin_count = int(2000)
+            bin_count = 2000
 
-        plt.hist(count_data, bins=bin_count, edgecolor='black', alpha=0.5)
+        plt.hist(count_data, bins=bin_count, edgecolor='black', 
+            alpha=0.5, label=filename)
 
     plt.xlabel(xlabel)
     plt.ylabel('Frequency (Number of Genes)')
     plt.title(title)
+    plt.legend(loc='upper right')
     plt.grid(False)
     plt.savefig(f"{output_file}.png", dpi=400)
     plt.close()
 
-
 def parse_args():
     """
-    Main function to set up argument prasing, handles input arguments, calls
+    Main function to set up argument parsing, handles input arguments, calls
     relevant functions for processing and plotting read count data.
-    
-    Args:
-
     """
     parser = argparse.ArgumentParser(
         prog="plot_readcount.py",        
-        description="Plot gene counts from a tab-separated file.")
+        description="Plot gene counts from tab-separated files.")
     
-    parser.add_argument("input_file", type=str, nargs='+',
-        help="Path to the input file containing gene count data.")
-    
+    parser.add_argument("input_files", type=str, nargs='+',
+        help="Paths to the input files containing gene count data.")
+
     parser.add_argument("output_file", type=str,
         help="Path to save the output plot as a PNG file.")
 
     parser.add_argument("--cutoff", type=int, default=None,
         help="Cutoff value for filtering gene counts.")
+
     parser.add_argument("--log2", action='store_true',
         help="Transform gene counts using log2.")
 
     return parser.parse_args()
 
 def main(args):
-    input_files = args.input_file
+    input_files = args.input_files
     output_file = args.output_file
     cutoff = args.cutoff
     log2_transform = args.log2
 
-    if log2_transform:
-        exclude_chromosomes = None
-    else:
+    exclude_chromosomes = None
+    if not log2_transform:
         exclude_chromosomes = ["mm12S", "mm16S", "mm45S", "mm4.5S", 
-            "mm5S", "mmBC1", "mmsnRNA", "RN7SK", "RN7SL", "RNU7", "RNY"]
+                               "mm5S", "mmBC1", "mmsnRNA", "RN7SK", 
+                               "RN7SL", "RNU7", "RNY"]
 
     dataframes = []
     for file in input_files:
         data = readfile(file, cutoff, exclude_chromosomes)
-        dataframes.append(data)
-    
+        filename = file.split(".bedtools_coverage.txt")[0]
+        dataframes.append((data, filename))
+        print(filename)
+        
     plot_gene_counts(dataframes, output_file, log2_transform)
 
 if __name__ == "__main__":
